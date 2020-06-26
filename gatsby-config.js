604f556cb8b06eae9d0f203c4f5c9dfadc4b46d7
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2020-06-26 12:43:41 +0530

    Change website title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `User Profiles`,
+    title: `Contentstack Personalization Demo`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
